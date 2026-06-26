b9188dcdcb0d07f93ceffcfe8c646d04df65150c
kotek <scpktk@gmail.com>
2026-06-26 12:51:49 +0900

    chore: 既定キャンバスサイズを400pxにする

--- research/scribing-lab/handwriting-collector/src/main.ts
+++ research/scribing-lab/handwriting-collector/src/main.ts
@@ -47,7 +47,7 @@ function readConfig(data: FormData): SetupConfig {
     writerId,
     charsetName: String(data.get("charset") ?? CHARSETS[0].name),
     rounds: clampInt(Number(data.get("rounds")), 1, 500, 10),
-    canvasSize: clampInt(Number(data.get("canvasSize")), 200, 1600, 800),
+    canvasSize: clampInt(Number(data.get("canvasSize")), 200, 1600, 400),
     penOnly: data.get("penOnly") === "on",
   };
 }
